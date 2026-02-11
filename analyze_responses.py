2ffb19dd5003c5bcbd4cf1ce1e99b2db8247db59
benthev <ben.thevathasan@gmail.com>
2026-02-10 18:03:55 -0600

    quant summaries in one function

--- analyze_responses.py
+++ analyze_responses.py
@@ -6,76 +6,60 @@ cols_quant = ['I felt like my voice mattered in this Seminar.',
               'I learned a lot from the Seminar.', 'How much fun did you have?',
               'Did it leave you wanting to learn more about this topic?']
 
-cols_qual = ['What is your impression of this Guide? Feel free to use 2-4 words (or phrases) to describe them.',
-             'What did you learn? What will stick with you from the Seminar?',
-             'What did you find most effective or enjoyable about the Guide and the Seminar they facilitated?',
-             "What didn't work for you about the Guide or the Seminar they facilitated? ",
-             'Let us know if you have more thoughts or feedback!']
+cols_qual = ['Let us know if you have more thoughts or feedback!']
 
 
-def guide_level_summary(df):
+def quant_summary(df, agg_cols):
     cols_quant_avail = [col for col in cols_quant if col in df.columns]
-    stats_means = df.groupby('Guide', dropna=False)[
+    stats_means = df.groupby(agg_cols, dropna=False)[
         cols_quant_avail].mean().round(3)
     stats_means['mean_overall'] = stats_means[cols_quant_avail].mean(
         axis=1).round(3)
     stats_counts = df.groupby(
-        'Guide', dropna=False).size().reset_index(name='count')
-    stats = pd.merge(stats_means, stats_counts, on='Guide',
+        agg_cols, dropna=False).size().reset_index(name='count')
+    stats = pd.merge(stats_means, stats_counts, on=agg_cols,
                      left_index=False, right_index=False)
     # Filter to topics with at least 5 responses
     stats = stats[stats['count'] >= 5]
     stats = stats.sort_values(by='mean_overall', ascending=False)
 
+    return stats
+
+
+def guide_level_summary(df):
+    stats = quant_summary(df, 'Guide')
     return stats
 
 
 def topic_level_summary(df):
-    cols_quant_avail = [col for col in cols_quant if col in df.columns]
-    stats_means = df.groupby(
-        'matched_topic', dropna=False)[cols_quant_avail].mean()
-    stats_means['mean_overall'] = stats_means[cols_quant_avail].mean(
-        axis=1).round(3)
-    stats_agg = df.groupby('matched_topic', dropna=False).agg(
-        count=('matched_topic', 'size'),
-        minimum_date=('Timestamp', 'min')
-    ).reset_index()
-    stats = pd.merge(stats_means, stats_agg, on='matched_topic',
-                     left_index=False, right_index=False)
-    # Filter to topics with at least 5 responses
-    stats = stats[stats['count'] >= 5]
-    stats = stats.sort_values(by='mean_overall', ascending=False)
-
+    stats = quant_summary(df, 'matched_topic')
     return stats
 
 
 def topic_guide_level_summary(df):
-    cols_quant_avail = [col for col in cols_quant if col in df.columns]
-    stats_means = df.groupby(
-        ['matched_topic', 'Guide'], dropna=False)[cols_quant_avail].mean()
-    stats_means['mean_overall'] = stats_means[cols_quant_avail].mean(
-        axis=1).round(3)
-    stats_agg = df.groupby(['matched_topic', 'Guide'], dropna=False).agg(
-        count=('matched_topic', 'size'),
-        minimum_date=('Timestamp', 'min')
-    ).reset_index()
-    stats = pd.merge(stats_means, stats_agg, on=['matched_topic', 'Guide'],
-                     left_index=False, right_index=False)
-    # Filter to topics with at least 5 responses
-    stats = stats[stats['count'] >= 5]
-    stats = stats.sort_values(by='mean_overall', ascending=False)
-
+    stats = quant_summary(df, ['matched_topic', 'Guide'])
     return stats
 
 
-def summarize_qualitative_feedback(df):
+def summarize_qualitative_feedback(df, agg_cols):
     cols_qual_avail = [col for col in cols_qual if col in df.columns]
-    summaries = {}
-    for col in cols_qual_avail:
-        feedback = df[col].dropna().tolist()
-        summarizer = SimpleTextSummarizer()
-        summaries[col] = summarizer.summarize_texts(feedback)
-    return summaries
+
+    if len(cols_qual_avail) > 1:
+        df['qual_feedback'] = df[cols_qual_avail].fillna(
+            '').agg(';'.join, axis=1)
+    else:
+        df['qual_feedback'] = df[cols_qual_avail[0]].fillna(
+            '') if cols_qual_avail else ''
+
+    agg_df = df.groupby(agg_cols, dropna=False)['qual_feedback'].apply(
+        lambda x: '; '.join(x)).reset_index()
+
+    # summaries = {}
+    # for col in cols_qual_avail:
+    #     feedback = df[col].dropna().tolist()
+    #     summarizer = SimpleTextSummarizer()
+    #     summaries[col] = summarizer.summarize_texts(feedback)
+    return agg_df
 
 
 def correlation_analysis(df):
